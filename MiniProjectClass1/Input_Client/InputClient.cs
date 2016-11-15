5802633b06efb191926179033c7392bd7c09e40e
kkourk <kkourk15@student.aau.dk>
2016-11-15 09:30:02 +0100

    input

---
+++ MiniProjectClass1/Input_Client/InputClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_Client
+{
+    class InputClient
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
