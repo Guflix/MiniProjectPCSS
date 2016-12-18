68426763f2961ff4449d129fe32ff16a9fbfe2e9
ingebronx <irossa14@student.aau.dk>
2016-12-18 18:25:18 +0100

    Kate's stuff

--- MiniProjectClass1/MiniProjectClass1/InputClient.cs
+++ MiniProjectClass1/MiniProjectClass1/InputClient.cs
@@ -7,92 +7,90 @@ using System.Net.Sockets;
 
 namespace MiniProjectClass1
 {
-      public class StateObject
+    public class StateObject
     {
         public Socket workSocket = null;
         public const int BufferSize = 256;
         public byte[] buffer = new byte[BufferSize];
         public StringBuilder sb = new StringBuilder();
-    }
-
-          class InputClient
+     }
+    
+    class InputClient
     {
-                public static string str = Console.ReadLine();
-                private const int port = 12345;
-                private static ManualResetEvent connectDone = new ManualResetEvent(false);
-                private static ManualResetEvent sendDone = new ManualResetEvent(false);
-                private static ManualResetEvent receiveDone = new ManualResetEvent(false);
+        public static string str = Console.ReadLine();
+        private const int port = 12345;
+        private static ManualResetEvent connectDone = new ManualResetEvent(false);
+        private static ManualResetEvent sendDone = new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone = new ManualResetEvent(false);
 
-                private static String response = String.Empty;
+        private static String response = String.Empty;
 
-                private static void StartClient()
-                {
-                    try
-                    {
-                        IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
-                        IPAddress ipAddress = ipHostInfo.AddressList[0];
-                        IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+        private static void StartClient()
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
 
-                        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                        client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), client);
-                        connectDone.WaitOne();
+                Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), client);
+                connectDone.WaitOne();
 
-                        str = FormatString(str);
-                        Send(client, str);
-                        sendDone.WaitOne();
+                str = FormatString(str);
+                Send(client, str);
+                sendDone.WaitOne();
 
-                     
-                        client.Shutdown(SocketShutdown.Both);
-                        client.Close();
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
 
-                    }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
 
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                    }
-                }
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;
+                client.EndConnect(ar);
+                Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
+                connectDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        
+        private static void Send(Socket client, String data)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
 
-                private static void ConnectCallback(IAsyncResult ar)
-                {
-                    try
-                    {
-                        Socket client = (Socket)ar.AsyncState;
-                        client.EndConnect(ar);
-                        Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
-                        connectDone.Set();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                    }
-                }
+            client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), client);
+        }
 
-                private static void Send(Socket client, String data)
-                {
-                    byte[] byteData = Encoding.ASCII.GetBytes(data);
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;
+                int bytesSent = client.EndSend(ar);
+                Console.WriteLine("Bytes sent {0} to server", bytesSent);
 
-                    client.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), client);
-                }
-
-                private static void SendCallback(IAsyncResult ar)
-                {
-                    try
-                    {
-                        Socket client = (Socket)ar.AsyncState;
-                        int bytesSent = client.EndSend(ar);
-                        Console.WriteLine("Bytes sent {0} to server", bytesSent);
-
-                        sendDone.Set();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                    }
-                }
+                sendDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
 
 
-          public static string FormatString(string str)       
+        public static string FormatString(string str)       
         {
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < str.Length; i++)
@@ -107,14 +105,11 @@ namespace MiniProjectClass1
         }
 
 
-
-                static void Main(string[] args)
-                {
-                    StartClient();                 
-                   Console.ReadKey();
-
-                }
-            }
-
+        static void Main(string[] args)
+        {
+            StartClient();                 
+            Console.ReadKey();
+        }
     }
+}
 
