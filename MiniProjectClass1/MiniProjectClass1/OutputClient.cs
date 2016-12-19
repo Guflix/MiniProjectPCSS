abad59af019c752a7b34e2d09c3a3951c186c51c
Helene35 <hhni14@student.aau.dk>
2016-12-19 12:05:07 +0100

    finished the output client - Helene

--- MiniProjectClass1/MiniProjectClass1/OutputClient.cs
+++ MiniProjectClass1/MiniProjectClass1/OutputClient.cs
@@ -1,12 +1,123 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace MiniProjectClass1
 {
+    /* public class StateObject
+    {
+        public Socket workSocket = null;
+        public const int BufferSize = 256;
+        public byte[] buffer = new byte[BufferSize];
+        public StringBuilder sb = new StringBuilder();
+    } */ 
+
     class OutputClient
     {
+        private const int port = 12345;
+        private static ManualResetEvent connectDone = 
+            new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone =
+            new ManualResetEvent(false);
+        private static String response = String.Empty;
+
+        private static void StartClient()
+        {
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.Resolve("172.24.146.104");
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                Socket client = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+                client.BeginConnect(remoteEP, 
+                    new AsyncCallback(ConnectCallback), client);
+                connectDone.WaitOne();
+
+                Receive(client);
+                receiveDone.WaitOne();
+
+                Console.WriteLine("Response: {0}", response);
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;
+                client.EndConnect(ar);
+                Console.WriteLine("Socket connected to {0}", 
+                    client.RemoteEndPoint.ToString());
+                connectDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static private void Receive(Socket client){
+            try
+            {
+                StateObject state = new StateObject();
+                state.workSocket = client;
+
+                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReceiveCallback), state);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                StateObject state = (StateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+
+                int bytesRead = client.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    state.sb.Append(Encoding.ASCII.GetString(
+                        state.buffer, 0, bytesRead));
+                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize,
+                        0, new AsyncCallback(ReceiveCallback), state);
+                }
+                else
+                {
+                    if (state.sb.Length > 1)
+                    {
+                        response = state.sb.ToString();
+                    }
+                    receiveDone.Set();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartClient();
+            Console.ReadKey();
+        }
     }
+}
 }
